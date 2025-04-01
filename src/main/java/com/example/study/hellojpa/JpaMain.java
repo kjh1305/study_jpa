5fb1e6214b6ae8445199ce87f90bdb54d94cf032
kimjunhyeong <cosyq1305@naver.com>
2025-04-01 21:49:50 +0900

    build gradle sourceSets 추가 // entity 스캔 베이스가 classes 부터 빌드시 meta-inf 경로 변경

---
+++ src/main/java/com/example/study/hellojpa/JpaMain.java
@@ -0,0 +1,35 @@
+package com.example.study.hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        try {
+            // 하나만 만들어야됨
+            EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("hello");
+
+            // 트랜잭션 단위로 사용
+            EntityManager entityManager = managerFactory.createEntityManager();
+
+            EntityTransaction transaction = entityManager.getTransaction();
+            transaction.begin(); // 트랜잭션 시작
+
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+
+            entityManager.persist(member); // 멤버 저장
+
+            transaction.commit();
+
+            entityManager.close();
+
+            managerFactory.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
