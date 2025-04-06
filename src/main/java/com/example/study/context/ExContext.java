b76a8eee3ccb5fd7933d39fe1cad30eba6e1e771
kimjunhyeong <cosyq1305@naver.com>
2025-04-06 12:57:00 +0900

    영속성 컨텍스트

--- src/main/java/com/example/study/context/ExContext.java
+++ src/main/java/com/example/study/context/ExContext.java
@@ -13,6 +13,13 @@ public class ExContext {
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
         try {
+            // find 시 영속성 컨텍스트에 1차캐시에 존재x 시 -> DB에서 조회 후 1차캐시에 저장
+            // find 시 영속성 컨텐스트 1차캐시에 존재시 -> DB에서 조회x 쿼리 안날라감
+            Member member1 = entityManager.find(Member.class, 1);
+            Member member2 = entityManager.find(Member.class, 1);
+            System.out.println("member1 == member2 : " + (member1 == member2)); // true
+            System.out.println("member1 == member2 : " + (member1.equals(member2))); // true
+
             // 비영속
             Member member = new Member();
             member.setId(3L);
